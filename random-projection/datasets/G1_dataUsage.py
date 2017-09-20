2ccde4f18a9e42323dc8b174cb47e0c39262fcb8
Guilherme Varela <guilhermevarela@hotmail.com>
2017-09-20 09:01:19 -0300

    g1: get_sentences added

--- random-projection/datasets/G1_dataUsage.py
+++ random-projection/datasets/G1_dataUsage.py
@@ -1,8 +1,13 @@
 import json
-
+corpora=[]
 with open("4000_g1_articles.json") as data_file:
     articles = json.load(data_file, encoding='utf-8')
-    for article in articles:
-        print article["URL"]
+    for article in articles:        
+				print article["URL"]
+				news= article["URL"].split('/')[-1]
+				news= news.split('.html')[0]
+				corpora.append(news)
+        # print news 
         #print article["TEXT"]
-    print len(articles)
+    print 'articles', len(articles)
+    print 'unique news', len(set(corpora))
