f09161b33b22b1de881df1877db2cf660201fb2f
Guilherme Varela <guilhermevarela@hotmail.com>
2017-09-25 13:08:24 -0300

    py36 -> master: all notebooks tested

--- random-projection/c code/script.sh
+++ random-projection/c code/script.sh
@@ -1,17 +1,14 @@
-./run distance --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt"
-
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 4
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 16
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 64
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 256
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 1024
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 4096
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "gaussian" --dimension 16384
-
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 4
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 16
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 64
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 256
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 1024
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 4096
-./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 30 --method "achlioptas" --dimension 16384
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 4
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 16
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 64
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 256
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 1024
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 4096
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "gaussian" --dimension 16384
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 4
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 16
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 64
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 256
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 1024
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 4096
+./run experiment --bow "g1_corpora.txt" --distance "g1_distance_matrix.txt" --time 10 --method "achlioptas" --dimension 16384
