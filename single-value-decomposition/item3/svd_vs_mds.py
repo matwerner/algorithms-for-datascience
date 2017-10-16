cce8f0437f7e802fb8fbd0f74eccfb45c3088042
Guilherme Varela <guilhermevarela@hotmail.com>
2017-10-16 15:43:15 -0200

    svd_vs_mds

--- single-value-decomposition/item3/svd_vs_mds.py
+++ single-value-decomposition/item3/svd_vs_mds.py
@@ -99,18 +99,12 @@ if __name__ == '__main__':
 	X = computeX(XXT,2)
 	print(X)
 
-	mds= MDS()
-	Y = mds.fit_transform(D)
-
-	# Xn = normalize(X)
-	# Yn = normalize(Y)
-
-	# print(Yn)
+	
 	Xr = rotate_counterclockwise(X, 180)
 
-	# import code; code.interact(local=dict(globals(), **locals()))
+	
 	Xr = normalize(Xr, origin='center')
-	# Yr = rotate_counterclockwise(Yn,  45)
+	
 	print(Xr)
 	img = plt.imread('../img/usamap.png')
 	print(img.shape)
@@ -119,9 +113,9 @@ if __name__ == '__main__':
 
 	# for images top-left is zero
 	# for algos origin is center
-	imgscale = 0.85*np.array([[width, -height]]) # 0.95 compensates for the frame
+	imgscale = 0.8*np.array([[width, -height]]) # 0.95 compensates for the frame
 	imgfactor= np.tile(imgscale, (n,1))
-	imgscale = np.array([[0.6*width, 0.6*height]])
+	imgscale = np.array([[0.55*width, 0.5*height]])
 	imgintercept= np.tile(imgscale, (n,1))
 	# print(Xr)
 	Xs = imgfactor * Xr + imgintercept#scaled X
@@ -134,23 +128,41 @@ if __name__ == '__main__':
 	ax.scatter(Xs[:,0], Xs[:,1], c='r', s=1)
 	for i, txt in enumerate(labels):
 		ax.annotate(txt, (Xs[i,0],Xs[i,1]), fontsize=6)
-
+	plt.title('SVD')
 	plt.show()
-	# ax[1].scatter(Yr[:,0], Yr[:,1], c='g')
 
-	# plt.set_title('SVD rotation cloud')	
-	# ax[1].set_title('MDS rotation cloud')
-	# plt.show()
-	
-	# f, ax = plt.subplots(2, 1, figsize=(6,8))
-	# ax[0].scatter(Xr[:,0], Xr[:,1])
-	# ax[1].scatter(Yr[:,0], Yr[:,1])
-	# for i, txt in enumerate(labels):
-	# 	ax[0].annotate(txt, (Xr[i,0],Xr[i,1]))
-	# 	ax[1].annotate(txt, (Yr[i,0],Yr[i,1]))
+	mds= MDS()
+	Y = mds.fit_transform(D)
 
-	# ax[0].set_title('SVD rotation cloud')	
-	# ax[1].set_title('MDS rotation cloud')
-	# plt.show()
+	Yr = rotate_counterclockwise(Y, 90)
+
 	
+	Yr = normalize(Yr, origin='center')
+	
+	print(Yr)
+	img = plt.imread('../img/usamap.png')
+	print(img.shape)
+	height= img.shape[0]
+	width = img.shape[1]
+
+	# for images top-left is zero
+	# for algos origin is center
+	imgscale = 0.8*np.array([[width, -height]]) # 0.95 compensates for the frame
+	imgfactor= np.tile(imgscale, (n,1))
+	imgscale = np.array([[0.55*width, 0.5*height]])
+	imgintercept= np.tile(imgscale, (n,1))
+	# print(Xr)
+	Ys = imgfactor * Yr + imgintercept#scaled X
+	print(Ys)
+	# fig = plt.figure(figsize=(6,9))
+	
+	# fig, ax = plt.subplots(2,1)
+	fig, ax = plt.subplots()
+	implot = ax.imshow(img, origin='upper')
+	ax.scatter(Ys[:,0], Ys[:,1], c='r', s=1)
+	for i, txt in enumerate(labels):
+		ax.annotate(txt, (Ys[i,0],Ys[i,1]), fontsize=6)
+	plt.title('MDS')
+	plt.show()
+
 	
