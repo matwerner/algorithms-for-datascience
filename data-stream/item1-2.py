596b4119595fd9bb4b90667a379e366a14c0a539
Daniel Specht <danielssmenezes@gmail.com>
2017-11-03 00:33:19 -0200

    Advances on item 2

--- data-stream/item1-2.py
+++ data-stream/item1-2.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import hashlib
 import json
+import utils
 
 np.random.seed(100)
 
@@ -17,51 +18,93 @@ def batchOfRandoms(batches,sizeOfRandomArray):
 
 # Passo 1 Gere uma sequência de tamanho N e calcule o total de numeros distintos criados.
 
-def estimate(batches,sizeOfRandomArray):
-    uniques = np.array([])
+def calculateUniques(batches,sizeOfRandomArray):
+    uniques = {}
     #for array in batchOfRandoms(batches = int(1e3), sizeOfRandomArray = int(1e8)):
+    # values for feedback processing
+    count = 0
+    feedbackCount = 0
+    total = batches*sizeOfRandomArray
+    percentFeedback = 0.1
+
     for array in batchOfRandoms(batches, sizeOfRandomArray):
-        uniques = np.unique(np.concatenate((uniques,array)))
+        for value in array:
+            uniques[value] = 1
+            
+            # feedback
+            count += 1
+            if count % int(percentFeedback*total) is 0:
+                feedbackCount+=1
+                print "%s  processado \n"%(feedbackCount*percentFeedback*100)
+
+
         #uniques = np.unique(array)
+
+    with open("uniquesOutput.txt") as uniquesOutput:
+        uniquesOutput.write(str(len(uniques)))
+
     return len(uniques)
 
 # Passo 2 Gere uma sequência de tamanho N e estime o total de numeros distintos criados através da
 # abordagem vista na aula. Estude o impacto de variar o numero de funções de hash.
 
-
-
-def FlajoletMartin(batches, sizeOfRandomArray):
+def FlajoletMartin(batches, sizeOfRandomArray,Nhashes,prime,size):
+    
     # Averiguar quais funções de hash
-    hashFunctions = [{"function":hashFunction1,"maximumTail":-1,"maximumTailNumber":None},
-                     {"function":hashFunction2,"maximumTail":-1,"maximumTailNumber":None}]
+    hashFunctions = []
+
+    for i in range(Nhashes):      
+        hashFunctions.append(HashFunction(prime,size))
+
+    # valuies for feedback processing
+    count = 0
+    feedbackCount = 0
+    total = batches*sizeOfRandomArray
+    percentFeedback = 0.1
 
     for array in batchOfRandoms(batches, sizeOfRandomArray):
+
         for value in array:
             for i in range(len(hashFunctions)):
-                hashFunction = hashFunctions[i]["function"]
-                tail = getTailLenght(hashFunction(int(value)))
+                hashFunctions[i].Hash(int(value))
 
-                if tail > hashFunctions[i]["maximumTail"]:
-                    hashFunctions[i]["maximumTail"] = tail
-                    hashFunctions[i]["maximumTailNumber"] = int(value)
-                    
+            # feedback
+            count += 1
+            if count % int(percentFeedback*total) is 0:
+                feedbackCount+=1
+                print "%s  processado \n"%(feedbackCount*percentFeedback*100)
+
+
+    # Results output...
+
+    def logValue(file,valueName,value):
+        file.write(str(valueName)+"---"+str(value)+"\n")
 
     with open("outputs.txt","w") as outputFile:
-        for hashFunction in hashFunctions:
-            outputFile.write( str(hashFunction["maximumTail"])+"---"+ str(hashFunction["maximumTailNumber"])+"\n" ) 
-        # results
+        for i in range(len(hashFunctions)):
+            logValue(outputFile,"Hash function",str(i))
+            logValue(outputFile,"Max tail",hashFunctions[i].maximumTail)
+            logValue(outputFile,"Maximum tail number",hashFunctions[i].maximumTailNumber)
 
+class HashFunction:
+    def __init__ (self,primeNumber,size):
+        self.primeNumber = primeNumber
+        # self.coefficient = coefficient
+        self.maximumTail = -1
+        self.maximumTailNumber = None
+        self._hashFunction = utils.universal_hash(primeNumber,size)
 
+    def Hash(self,value):
+        hashValue = self._hashFunction(value)
+        hashValueTail = getTailLenght(hashValue)
+
+        if hashValueTail > self.maximumTail:
+            self.maximumTail = hashValueTail
+            self.maximumTailNumber = value
+
+        return hashValue
     
 # http://www.planetmath.org/goodhashtableprimes
-def standardHashFunction(primeNumber, value,coefficient=1):
-    return (coefficient*value)%primeNumber
-    
-def hashFunction1(value):
-    return standardHashFunction(3145739,value,3)
-
-def hashFunction2(value):
-    return standardHashFunction(12582917,value,4)
 
 def getTailLenght(intValue):
     count = 0
@@ -72,13 +115,12 @@ def getTailLenght(intValue):
     while (intValue & v) is  0:
         count += 1
         v =  v<<1
-
     return count
 
 
 #testes
-print getTailLenght(64)
+#print getTailLenght(64)
 
-print estimate(2,int(1e3))
+print calculateUniques(int(1e3),int(1e8))
 
-FlajoletMartin(2, int(1e3))
+#FlajoletMartin(batches= 1,sizeOfRandomArray= int(1e7),Nhashes=2**8,prime=1610612741,size=1)
