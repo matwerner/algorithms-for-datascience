bf608212451639cd20bb1d2d48fec3da975b7347
Daniel Specht <danielssmenezes@gmail.com>
2017-11-03 03:42:45 -0200

    Performance changes

--- data-stream/item1-2.py
+++ data-stream/item1-2.py
@@ -4,6 +4,7 @@ import sys
 import hashlib
 import json
 import utils
+import pyhash
 
 np.random.seed(100)
 
@@ -35,7 +36,7 @@ def calculateUniques(batches,sizeOfRandomArray):
             count += 1
             if count % int(percentFeedback*total) is 0:
                 feedbackCount+=1
-                print "%s  processado \n"%(feedbackCount*percentFeedback*100)
+                print ("%s  processado \n"%(feedbackCount*percentFeedback*100))
 
 
         #uniques = np.unique(array)
@@ -48,7 +49,7 @@ def calculateUniques(batches,sizeOfRandomArray):
 # Passo 2 Gere uma sequência de tamanho N e estime o total de numeros distintos criados através da
 # abordagem vista na aula. Estude o impacto de variar o numero de funções de hash.
 
-def FlajoletMartin(batches, sizeOfRandomArray,Nhashes,prime,size):
+def FlajoletMartin(arraySize,N,Nhashes,prime,size):
     
     # Averiguar quais funções de hash
     hashFunctions = []
@@ -59,32 +60,33 @@ def FlajoletMartin(batches, sizeOfRandomArray,Nhashes,prime,size):
     # valuies for feedback processing
     count = 0
     feedbackCount = 0
-    total = batches*sizeOfRandomArray
-    percentFeedback = 0.1
+    percentFeedback = 0.01
 
-    for array in batchOfRandoms(batches, sizeOfRandomArray):
-
+    for array in utils.random_data_stream(arraySize,N):
         for value in array:
             for i in range(len(hashFunctions)):
-                hashFunctions[i].Hash(int(value))
+                hashFunctions[i].Hash(value)
 
             # feedback
-            count += 1
-            if count % int(percentFeedback*total) is 0:
-                feedbackCount+=1
-                print "%s  processado \n"%(feedbackCount*percentFeedback*100)
+            # count += 1
+            # if count % int(percentFeedback*N) is 0:
+            #     feedbackCount+=1
+            #     print ("%s  processado \n"%(feedbackCount*percentFeedback*100))
 
 
     # Results output...
 
     def logValue(file,valueName,value):
-        file.write(str(valueName)+"---"+str(value)+"\n")
+        file.write(str(valueName)+"\t"+str(value)+"\n")
 
     with open("outputs.txt","w") as outputFile:
         for i in range(len(hashFunctions)):
-            logValue(outputFile,"Hash function",str(i))
-            logValue(outputFile,"Max tail",hashFunctions[i].maximumTail)
-            logValue(outputFile,"Maximum tail number",hashFunctions[i].maximumTailNumber)
+            logValue(outputFile,"HashFunction",str(i))
+            logValue(outputFile,"MaxTail",hashFunctions[i].maximumTail)
+            logValue(outputFile,"MaximumTailNumber",hashFunctions[i].maximumTailNumber)
+
+
+HASHER = pyhash.fnv1_64()
 
 class HashFunction:
     def __init__ (self,primeNumber,size):
@@ -92,7 +94,12 @@ class HashFunction:
         # self.coefficient = coefficient
         self.maximumTail = -1
         self.maximumTailNumber = None
+        
+        #a = np.random.randint(1, primeNumber)        
+        #self._hashFunction =lambda x: (a+HASHER(x.tobytes()))
+       
         self._hashFunction = utils.universal_hash(primeNumber,size)
+        #self._hashFunction = lambda x: (x)
 
     def Hash(self,value):
         hashValue = self._hashFunction(value)
@@ -121,6 +128,9 @@ def getTailLenght(intValue):
 #testes
 #print getTailLenght(64)
 
-print calculateUniques(int(1e3),int(1e8))
+#print calculateUniques(int(1e4),int(1e7))
 
-#FlajoletMartin(batches= 1,sizeOfRandomArray= int(1e7),Nhashes=2**8,prime=1610612741,size=1)
+FlajoletMartin(arraySize = int(1e7),N= int(1e10),Nhashes=2**8,prime=1610612741,size=1)
+
+#print utils.item1(int(1e7),int(1e10))
+#FlajoletMartin(arraySize = int(1e7),N= int(1e10),Nhashes=2**8,prime=1610612741,size=1)
