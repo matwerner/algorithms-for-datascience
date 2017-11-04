da54c3d7cee390cc6ecfde9524653762da38cf23
Matheus Telles <matheustwer@gmail.com>
2017-11-04 11:29:19 -0200

    Change to the required data stream size

--- data-stream/c code/script.sh
+++ data-stream/c code/script.sh
@@ -1,7 +1,7 @@
 # Declare stream parameters
-BATCH_SIZE=1
+BATCH_SIZE=10
 STREAM_SIZE=100000000
 # Run experiments
 time ./Run Moment --batch $BATCH_SIZE --stream $STREAM_SIZE --output "Results/ZerothMoment.txt" --nth 0
 time ./Run Moment --batch $BATCH_SIZE --stream $STREAM_SIZE --output "Results/SecondMoment.txt" --nth 2
-time ./Run AMS --batch $BATCH_SIZE --stream $STREAM_SIZE --output "Results/AMS.txt" --variable 1024
+time ./Run AMS --batch $BATCH_SIZE --stream $STREAM_SIZE --output "Results/AMS.txt" --variable 8096
