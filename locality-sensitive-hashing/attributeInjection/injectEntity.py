7db10f84fa9a68d8feca41b7ba80c1b7993d6928
Daniel Specht <danielssmenezes@gmail.com>
2017-12-10 23:47:31 -0200

    Lookup table for named entities

--- locality-sensitive-hashing/attributeInjection/injectEntity.py
+++ locality-sensitive-hashing/attributeInjection/injectEntity.py
@@ -2,73 +2,72 @@ from polyglot.text import Text
 import polyglot
 import json
 import spacy
-
 nlp = spacy.load('pt')
+
+person ="person"
+organisation = "organisation"
+place = "place"
 
 def structureText(text):
     """ Removes literals \n \t \r"""
-    return text.replace("\n","").replace("\t","").replace("\r","")    
+    return ' '.join(text.replace("\n"," ").replace("\t"," ").replace("\r"," ").replace("\v"," ").replace("\n"," ").replace("\f"," ").split())
 
 # input example: {"title":"geologo" }
 def getEntities(stringDict):
     entitiesByModel = {}
+    for model, func in NerModels.items():
+        entitiesByModel[model] = {}
+        for key, text in stringDict.items():
+            if key != "id" and text != "" :
+                entitiesByModel[model][key] = func(text,stringDict["id"])
+    return entitiesByModel
 
-    for key1, func in NerModels.items():
-        entitiesByModel[key1] = {}
-        for key2, text in stringDict.items():
-            entitiesByModel[key1][key2] = func(text)
-    return entitiesByModel 
-
-
-def NerPolyglot(sentence):
-    entities = {"Person":[],"Organisation":[],"Place":[]}
-
-    polyglotAnnotation = Text(sentence, hint_language_code='pt')    
+def NerPolyglot(text,id):
+    entities = {person:[],organisation:[],place:[]}
+    polyglotAnnotation = Text(text, hint_language_code='pt')
     for entity in polyglotAnnotation.entities:
         entityStr = list(polyglotAnnotation.words[entity.start:entity.end])
-        if entity.tag == "I-PER":
-            entities["Person"].append(entityStr)
+        # if entity.tag == "I-PER":
+        #     entities[person].append(entityStr)
         if entity.tag == "I-LOC":
-            entities["Place"].append(entityStr)
+            entities[place].append(entityStr)
         if entity.tag == "I-ORG":
-            entities["Organisation"].append(entityStr)
-
+            entities[organisation].append(entityStr)
     return entities
 
-def NerSpacy(sentence):
-    entities = {"Person":[],"Organisation":[],"Place":[]}
-    doc = nlp(sentence)
-
+def NerSpacy(text,id):
+    entities = {person:[],organisation:[],place:[]}
+    doc = nlp( structureText(text))
     # Find named entities, phrases and concepts
     for entity in doc.ents:
-        if entity.label_ == "PER":
-            entities["Person"].append(entity.text)
+    #     if entity.label_ == "PER":
+    #         entities[person].append(entity.text)
+
         if entity.label_ == "LOC":
-            entities["Place"].append(entity.text)
+            entities[place].append(entity.text)
+
         if entity.label_ == "ORG":
-            entities["Organisation"].append(entity.text)
+            entities[organisation].append(entity.text)
 
     return entities
 
-if __name__ == '__main__':
-    global NerModels 
-    NerModels = {"Spacy":NerSpacy}
-
-
+def injectEntities(outputJson,inputJson):
     with open("../datasets/development.json","r") as f:
         vagas = json.load(f)
         output = {}
 
-        for vaga in vagas:            
+        for vaga in vagas[9700:]:
             title = structureText(vaga["title"])
-            description = structureText(vaga["description"])
-
-            entidadesVaga = getEntities({"title":title,"description":description})
+            entidadesVaga = getEntities(vaga)
             
             # Indexa os resultados pelo id da vaga
             output[vaga["id"]] = entidadesVaga
             print (vaga["id"])
 
-        with open("entities.json","w") as entities:
+        with open(outputJson,"w") as entities:
             entities.write(json.dumps(output))
 
+if __name__ == '__main__':
+    global NerModels 
+    NerModels = {"Spacy":NerSpacy,"Polyglot":NerPolyglot }
+    injectEntities("documents_entities.json","../datasets/development.json")
